cc56319e5398069e1eb273b5a7e2419ac118b69d
kot2d <kot2d@outlook.com>
2024-11-01 12:45:24 +0300

    second commit

---
+++ src/main/java/com/testgame/wall_and_cannons_server/WallAndCannonsServerApplication.java
@@ -0,0 +1,13 @@
+package com.testgame.wall_and_cannons_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallAndCannonsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WallAndCannonsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testgame/wall_and_cannons_server/WallAndCannonsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testgame.wall_and_cannons_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallAndCannonsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
