dcafbc0adec4246b6873d345bbcef446c1ff8d37
kot2d <kot2d@outlook.com>
2024-11-01 22:00:07 +0300

    added services

---
+++ src/main/java/com/testgame/wall_and_cannons_server/services/CellService.java
@@ -0,0 +1,33 @@
+package com.testgame.wall_and_cannons_server.services;
+
+import com.testgame.wall_and_cannons_server.domain.Cell;
+import com.testgame.wall_and_cannons_server.domain.Wall;
+import com.testgame.wall_and_cannons_server.persistance.CellRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CellService {
+
+    @Autowired
+    CellRepository cellRepository;
+
+    public Wall buildCells(Wall wall) {
+        int rows = wall.getWallRows();
+        int columns = wall.getWallColumns();
+        for(int i = 0; i < rows; i++) {
+            int temp_columns = columns;
+            if(wall.isShortRow(i)) {
+                temp_columns++;
+            }
+            for(int j = 0; j < temp_columns; j++) {
+                Cell cell = new Cell();
+                cell.setCellRow(i);
+                cell.setCellRow(j);
+                cell.setWall(wall);
+                cellRepository.save(cell);
+            }
+        }
+        return wall;
+    }
+}
---
+++ src/main/java/com/testgame/wall_and_cannons_server/services/GameService.java
@@ -0,0 +1,46 @@
+package com.testgame.wall_and_cannons_server.services;
+
+import com.testgame.wall_and_cannons_server.controllers.UserRequestBody;
+import com.testgame.wall_and_cannons_server.domain.Castle;
+import com.testgame.wall_and_cannons_server.domain.PlayerUser;
+import com.testgame.wall_and_cannons_server.domain.Wall;
+import com.testgame.wall_and_cannons_server.persistance.CastleRepository;
+import com.testgame.wall_and_cannons_server.persistance.CellRepository;
+import com.testgame.wall_and_cannons_server.persistance.UserRepository;
+import com.testgame.wall_and_cannons_server.persistance.WallRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GameService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    CastleRepository castleRepository;
+
+    @Autowired
+    WallRepository wallRepository;
+
+    @Autowired
+    CellService cellService;
+
+
+    public UserRequestBody createNewPlayer(UserRequestBody newUser) {
+        PlayerUser user = userRepository.save(newUser.mapToPlayerUser());
+
+        Wall wall = new Wall();
+        wall.setWallColumns(5);
+        wall.setWallRows(5);
+        wallRepository.save(wall);
+        cellService.buildCells(wall);
+
+        Castle castle = new Castle();
+        castle.setPlayerUser(user);
+        castle.setWall(wall);
+        castleRepository.save(castle);
+
+        return new UserRequestBody(user);
+    }
+}
