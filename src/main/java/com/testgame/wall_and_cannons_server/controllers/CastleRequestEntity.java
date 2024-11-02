907c109bbe4f110cfd9a2d5310e87844bdbd3707
kot2d <kot2d@outlook.com>
2024-11-02 16:02:29 +0300

    added class for request entity

---
+++ src/main/java/com/testgame/wall_and_cannons_server/controllers/CastleRequestEntity.java
@@ -0,0 +1,23 @@
+package com.testgame.wall_and_cannons_server.controllers;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.testgame.wall_and_cannons_server.domain.Castle;
+
+public class CastleRequestEntity {
+
+    public CastleRequestEntity(Castle castle) {
+        this.id = castle.getId();
+        this.wallId = castle.getWall().getId();
+        this.userId = castle.getPlayerUser().getId();
+    }
+
+    @JsonProperty("id")
+    public long id;
+
+    @JsonProperty("user_id")
+    public long userId;
+
+    @JsonProperty("wall_id")
+    public long wallId;
+
+}
