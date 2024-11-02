d265038f056468d90d0fada5bd7b13bf12bb4095
kot2d <kot2d@outlook.com>
2024-11-02 16:02:03 +0300

    added method findByPlayerUserId

--- src/main/java/com/testgame/wall_and_cannons_server/persistance/CastleRepository.java
+++ src/main/java/com/testgame/wall_and_cannons_server/persistance/CastleRepository.java
@@ -1,8 +1,11 @@
 package com.testgame.wall_and_cannons_server.persistance;
 
 import com.testgame.wall_and_cannons_server.domain.Castle;
-import com.testgame.wall_and_cannons_server.domain.PlayerUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CastleRepository extends JpaRepository<Castle, Long> {
+
+    List<Castle> findByPlayerUserId(Long playerUserId);
 }
