a398bca8a6fb56573e40e5e498c25370e14dc152
kot2d <kot2d@outlook.com>
2024-11-02 16:04:15 +0300

    added parameter of wall id for method getHitCells

--- src/main/java/com/testgame/wall_and_cannons_server/controllers/CellController.java
+++ src/main/java/com/testgame/wall_and_cannons_server/controllers/CellController.java
@@ -1,8 +1,10 @@
 package com.testgame.wall_and_cannons_server.controllers;
 
+import com.testgame.wall_and_cannons_server.domain.Cell;
 import com.testgame.wall_and_cannons_server.services.CellService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +17,9 @@ public class CellController {
     @Autowired
     CellService cellService;
 
-    @GetMapping("/hit")
-    public List<CellRequestEntity> getHitCells() {
-        return cellService.getCellsByWall(2).stream().map(CellRequestEntity::new).toList();
+    @GetMapping("/hit/{wall_id}")
+    public List<CellRequestEntity> getHitCells(@PathVariable("wall_id") Long wallId) {
+        return cellService.getCellsByWall(wallId).stream().map(CellRequestEntity::new).toList();
     }
 
 
