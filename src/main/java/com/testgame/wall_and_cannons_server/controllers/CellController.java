894a3706f2a108017319ed3e072d6f4ca05f502f
kot2d <kot2d@outlook.com>
2024-11-02 13:01:27 +0300

    added method getHitCells

--- src/main/java/com/testgame/wall_and_cannons_server/controllers/CellController.java
+++ src/main/java/com/testgame/wall_and_cannons_server/controllers/CellController.java
@@ -1,5 +1,24 @@
 package com.testgame.wall_and_cannons_server.controllers;
 
+import com.testgame.wall_and_cannons_server.services.CellService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cell")
 public class CellController {
+
+    @Autowired
+    CellService cellService;
+
+    @GetMapping("/hit")
+    public List<CellRequestEntity> getHitCells() {
+        return cellService.getCellsByWall(2).stream().map(CellRequestEntity::new).toList();
+    }
+
 
 }
