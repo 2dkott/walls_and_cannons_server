98a008a197fc939b0d7c35051a17260d35f4f265
kot2d <kot2d@outlook.com>
2024-11-03 20:10:26 +0300

    added battle controller

---
+++ src/main/java/com/testgame/wall_and_cannons_server/controllers/BattleController.java
@@ -0,0 +1,11 @@
+package com.testgame.wall_and_cannons_server.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/battle")
+public class BattleController {
+
+
+}
