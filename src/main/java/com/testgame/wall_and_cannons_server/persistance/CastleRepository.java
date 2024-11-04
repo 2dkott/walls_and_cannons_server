ec8a4f6aacfa1af420c5666e64902e6d493d5f1b
kot2d <kot2d@outlook.com>
2024-11-04 19:12:27 +0300

    added method findCastleByPlayerUserAndAndCurrentIsTrue

--- src/main/java/com/testgame/wall_and_cannons_server/persistance/CastleRepository.java
+++ src/main/java/com/testgame/wall_and_cannons_server/persistance/CastleRepository.java
@@ -5,8 +5,11 @@ import com.testgame.wall_and_cannons_server.domain.PlayerUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CastleRepository extends JpaRepository<Castle, Long> {
 
     List<Castle> findByPlayerUser(PlayerUser playerUser);
+
+    Optional<Castle> findCastleByPlayerUserAndAndCurrentIsTrue(PlayerUser playerUser);
 }
