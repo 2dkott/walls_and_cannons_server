3f4211a73a18070d79281375435087492eae52de
kot2d <kot2d@outlook.com>
2024-11-04 19:13:43 +0300

    update logic of create battle

--- src/main/java/com/testgame/wall_and_cannons_server/services/BattleService.java
+++ src/main/java/com/testgame/wall_and_cannons_server/services/BattleService.java
@@ -3,6 +3,7 @@ package com.testgame.wall_and_cannons_server.services;
 import com.testgame.wall_and_cannons_server.domain.Battle;
 import com.testgame.wall_and_cannons_server.domain.BattleRoundCell;
 import com.testgame.wall_and_cannons_server.domain.Castle;
+import com.testgame.wall_and_cannons_server.domain.PlayerUser;
 import com.testgame.wall_and_cannons_server.persistance.BattleCellRepository;
 import com.testgame.wall_and_cannons_server.persistance.BattleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +23,10 @@ public class BattleService {
     @Autowired
     private CastleService castleService;
 
-    public Battle createBattle(Castle castleA, Castle castleB) {
+    public Battle createBattle(PlayerUser playerUserA, PlayerUser playerUserB) {
         Battle battle = new Battle();
+
+
         battle.setPlayerUserA(castleA.getPlayerUser());
         battle.setPlayerUserB(castleB.getPlayerUser());
         battleRepository.save(battle);
