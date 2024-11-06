16b675bbdf83ca960bf70e4311a2aeddfa8da8d6
kot2d <kot2d@outlook.com>
2024-11-06 23:20:29 +0300

    removed useless methods

--- src/main/java/com/testgame/wall_and_cannons_server/services/BattleService.java
+++ src/main/java/com/testgame/wall_and_cannons_server/services/BattleService.java
@@ -1,10 +1,7 @@
 package com.testgame.wall_and_cannons_server.services;
 
 import com.testgame.wall_and_cannons_server.domain.Battle;
-import com.testgame.wall_and_cannons_server.domain.BattleRoundCell;
-import com.testgame.wall_and_cannons_server.domain.Castle;
 import com.testgame.wall_and_cannons_server.domain.PlayerUser;
-import com.testgame.wall_and_cannons_server.persistance.BattleCellRepository;
 import com.testgame.wall_and_cannons_server.persistance.BattleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -17,26 +14,11 @@ public class BattleService {
     @Autowired
     private BattleRepository battleRepository;
 
-    @Autowired
-    private BattleCellRepository battleCellRepostitory;
-
-    @Autowired
-    private CastleService castleService;
-
-    public Battle createBattle(PlayerUser playerUserA, PlayerUser playerUserB) {
-        Battle battle = new Battle();
-
-
-        battle.setPlayerUserA(castleA.getPlayerUser());
-        battle.setPlayerUserB(castleB.getPlayerUser());
-        battleRepository.save(battle);
-
-        List<BattleRoundCell> battleRoundCells = castleA.getWall().getCellList().stream()
-                .map(cell -> new BattleRoundCell(battle, cell)).toList();
-        battleCellRepostitory.saveAll(battleRoundCells);
-
-
-
-        return battle;
+    public List<Battle> getBattlesByPlayerUser(PlayerUser playerUser, boolean isConfirmed, boolean isFinished) {
+        return battleRepository.findAll().stream()
+                .filter(battle -> battle.isFinished == isFinished)
+                .filter(battle -> battle.isByPlayer(playerUser))
+                .filter(battle -> battle.getPartyByPlayer(playerUser).get().isConfirmed() == isConfirmed)
+                .toList();
     }
 }
