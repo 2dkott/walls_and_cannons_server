0088a88bca7601128e5665c210fbbac522f870ca
kot2d <kot2d@outlook.com>
2024-11-07 19:54:13 +0300

    added class to store a player party for battle

---
+++ src/main/java/com/testgame/wall_and_cannons_server/domain/PlayerParty.java
@@ -0,0 +1,25 @@
+package com.testgame.wall_and_cannons_server.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table
+public class PlayerParty {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "PlayerUser_id", nullable = false)
+    private PlayerUser playerUser;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "Battle_id", nullable = false)
+    private Battle battle;
+
+    private boolean isConfirmed;
+}
