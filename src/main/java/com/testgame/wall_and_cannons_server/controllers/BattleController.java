7739d3d89c95ce90623e94cd40803314433bf596
kot2d <kot2d@outlook.com>
2024-11-07 19:55:36 +0300

    added method findBattle

--- src/main/java/com/testgame/wall_and_cannons_server/controllers/BattleController.java
+++ src/main/java/com/testgame/wall_and_cannons_server/controllers/BattleController.java
@@ -1,5 +1,14 @@
 package com.testgame.wall_and_cannons_server.controllers;
 
+import com.testgame.wall_and_cannons_server.domain.PlayerUser;
+import com.testgame.wall_and_cannons_server.exceptions.NoUserFoundException;
+import com.testgame.wall_and_cannons_server.services.ActiveUserProvider;
+import com.testgame.wall_and_cannons_server.services.BattleMatcher;
+import com.testgame.wall_and_cannons_server.services.BattleService;
+import com.testgame.wall_and_cannons_server.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +16,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/battle")
 public class BattleController {
 
+
+    @Autowired
+    BattleMatcher battleMatcher;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    ActiveUserProvider activeUserProvider;
+
+    @Autowired
+    BattleService battleService;
+
+    @GetMapping("/find/{userId}")
+    public void findBattle(@PathVariable long userId) {
+        PlayerUser playerUser = userService.getUserById(userId).orElseThrow(() -> new NoUserFoundException(userId));
+
+        //if(battleService.getUnConfirmedBattlesByPlayerUser(playerUser).isEmpty());
+
+    }
 
 }
