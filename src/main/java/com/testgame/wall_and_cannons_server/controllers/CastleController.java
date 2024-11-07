6e0990b5514a61ebbd83947eba78db69063e3a99
kot2d <kot2d@outlook.com>
2024-11-07 19:56:40 +0300

    updated method getAllCastles

--- src/main/java/com/testgame/wall_and_cannons_server/controllers/CastleController.java
+++ src/main/java/com/testgame/wall_and_cannons_server/controllers/CastleController.java
@@ -1,7 +1,9 @@
 package com.testgame.wall_and_cannons_server.controllers;
 
-import com.testgame.wall_and_cannons_server.domain.Castle;
+import com.testgame.wall_and_cannons_server.domain.PlayerUser;
+import com.testgame.wall_and_cannons_server.exceptions.NoUserFoundException;
 import com.testgame.wall_and_cannons_server.services.CastleService;
+import com.testgame.wall_and_cannons_server.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,9 +16,13 @@ public class CastleController {
     @Autowired
     private CastleService castleService;
 
+    @Autowired
+    UserService userService;
+
     @GetMapping("/{user_id}")
     public List<CastleRequestEntity> getAllCastles(@PathVariable("user_id") Long userId) {
-        return castleService.getAllCastlesByUserId(userId).stream().map(CastleRequestEntity::new).toList();
+        PlayerUser playerUser = userService.getUserById(userId).orElseThrow(() -> new NoUserFoundException(userId));
+        return castleService.getAllCastlesByUser(playerUser).stream().map(CastleRequestEntity::new).toList();
     }
 }
 
