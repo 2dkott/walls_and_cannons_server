36d579ef8b9c49cd7dad34a5c8b20e3c289d428c
kot2d <kot2d@outlook.com>
2024-11-07 19:54:33 +0300

    added method getUserById

--- src/main/java/com/testgame/wall_and_cannons_server/services/UserService.java
+++ src/main/java/com/testgame/wall_and_cannons_server/services/UserService.java
@@ -1,8 +1,11 @@
 package com.testgame.wall_and_cannons_server.services;
 
+import com.testgame.wall_and_cannons_server.domain.PlayerUser;
 import com.testgame.wall_and_cannons_server.persistance.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class UserService {
@@ -10,6 +13,7 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-
-
+    public Optional<PlayerUser> getUserById(long id) {
+        return userRepository.findById(id);
+    }
 }
