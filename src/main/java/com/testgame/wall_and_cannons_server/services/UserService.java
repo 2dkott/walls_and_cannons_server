f6043a66b2d65f46c53b101284df5cfc4262e837
kot2d <kot2d@outlook.com>
2024-11-10 00:10:58 +0300

    added logging of searching user by id

--- src/main/java/com/testgame/wall_and_cannons_server/services/UserService.java
+++ src/main/java/com/testgame/wall_and_cannons_server/services/UserService.java
@@ -2,18 +2,21 @@ package com.testgame.wall_and_cannons_server.services;
 
 import com.testgame.wall_and_cannons_server.domain.PlayerUser;
 import com.testgame.wall_and_cannons_server.persistance.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+@Slf4j
 @Component
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
 
-    public Optional<PlayerUser> getUserById(long id) {
-        return userRepository.findById(id);
+    public Optional<PlayerUser> getUserById(long userId) {
+        log.info("Searching Player by userId: {}", userId);
+        return userRepository.findById(userId);
     }
 }
