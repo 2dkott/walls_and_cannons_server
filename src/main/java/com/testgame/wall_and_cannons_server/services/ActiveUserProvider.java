dfb9574aab56efd23868bb1bfa7ef383ae363d64
kot2d <kot2d@outlook.com>
2024-11-10 16:30:29 +0300

    update method putUser

--- src/main/java/com/testgame/wall_and_cannons_server/services/ActiveUserProvider.java
+++ src/main/java/com/testgame/wall_and_cannons_server/services/ActiveUserProvider.java
@@ -1,13 +1,13 @@
 package com.testgame.wall_and_cannons_server.services;
 
 import com.testgame.wall_and_cannons_server.domain.PlayerUser;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.Random;
 
+@Slf4j
 @Component
 public class ActiveUserProvider {
 
@@ -21,8 +21,16 @@ public class ActiveUserProvider {
         return activePlayers.isEmpty();
     }
 
-    public void addUser(PlayerUser user) {
-        activePlayers.add(user);
+    public void putUser(PlayerUser user) {
+        log.info("Putting {} to active list", user);
+        if (!activePlayers.contains(user)) {
+            activePlayers.add(user);
+        }
+    }
+
+    public boolean isUserInList(PlayerUser user) {
+        log.info("Is {} in active list", user);
+        return activePlayers.contains(user);
     }
 
 }
