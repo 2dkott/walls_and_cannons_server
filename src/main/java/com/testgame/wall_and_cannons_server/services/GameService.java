0d7ec30a53bef65c144b78e5b1ec5ec3fde8706c
kot2d <kot2d@outlook.com>
2024-11-11 19:54:35 +0300

    added createBattle method

--- src/main/java/com/testgame/wall_and_cannons_server/services/GameService.java
+++ src/main/java/com/testgame/wall_and_cannons_server/services/GameService.java
@@ -1,15 +1,18 @@
 package com.testgame.wall_and_cannons_server.services;
 
 import com.testgame.wall_and_cannons_server.controllers.UserRequestEntity;
+import com.testgame.wall_and_cannons_server.domain.Battle;
 import com.testgame.wall_and_cannons_server.domain.Castle;
 import com.testgame.wall_and_cannons_server.domain.PlayerUser;
 import com.testgame.wall_and_cannons_server.domain.Wall;
 import com.testgame.wall_and_cannons_server.persistance.CastleRepository;
 import com.testgame.wall_and_cannons_server.persistance.UserRepository;
 import com.testgame.wall_and_cannons_server.persistance.WallRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class GameService {
 
@@ -25,21 +28,39 @@ public class GameService {
     @Autowired
     CellService cellService;
 
+    @Autowired
+    BattleService battleService;
+
+    @Autowired
+    PlayerPartyService playerPartyService;
+
 
     public UserRequestEntity createNewPlayer(UserRequestEntity newUser) {
+        log.info("Creating new {} from request", newUser);
         PlayerUser user = userRepository.save(newUser.mapToPlayerUser());
+        log.info("{} was saved in DataBase", user);
+
+        Castle castle = new Castle();
+        castle.setPlayerUser(user);
+        castleRepository.save(castle);
+        log.info("{} was saved in DataBase", castle);
 
         Wall wall = new Wall();
         wall.setWallColumns(5);
         wall.setWallRows(5);
+        wall.setCastle(castle);
         wallRepository.save(wall);
+        log.info("{} was saved in DataBase", wall);
         cellService.buildCells(wall);
 
-        Castle castle = new Castle();
-        castle.setPlayerUser(user);
-        castle.setWall(wall);
-        castleRepository.save(castle);
-
         return new UserRequestEntity(user);
+    }
+
+    public Battle createBattle(Battle battle) {
+        battle = battleService.saveBattle(battle);
+        battle.getPlayerParties().forEach(playerParty -> {
+            playerPartyService.save(playerParty);
+        });
+        return battle;
     }
 }
