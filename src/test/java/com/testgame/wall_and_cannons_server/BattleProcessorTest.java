0a263efae297e94405ee777307c84363e82da551
kot2d <kot2d@outlook.com>
2024-11-28 23:49:29 +0300

    added tests for testGetLatestRound

--- src/test/java/com/testgame/wall_and_cannons_server/BattleProcessorTest.java
+++ src/test/java/com/testgame/wall_and_cannons_server/BattleProcessorTest.java
@@ -1,23 +1,26 @@
 package com.testgame.wall_and_cannons_server;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import com.testgame.wall_and_cannons_server.domain.Battle;
 import com.testgame.wall_and_cannons_server.domain.BattleProcessor;
 import com.testgame.wall_and_cannons_server.domain.BattleRound;
 import com.testgame.wall_and_cannons_server.domain.PlayerParty;
 import com.testgame.wall_and_cannons_server.domain.PlayerUser;
+import com.testgame.wall_and_cannons_server.exceptions.NoRoundsForBattleException;
 import com.testgame.wall_and_cannons_server.persistance.BattleRepository;
 import com.testgame.wall_and_cannons_server.persistance.BattleRoundRepository;
 import com.testgame.wall_and_cannons_server.persistance.PlayerPartyRepository;
 import com.testgame.wall_and_cannons_server.services.BattleService;
 import com.testgame.wall_and_cannons_server.services.PlayerPartyService;
-import jakarta.annotation.PostConstruct;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
@@ -113,22 +116,115 @@ public class BattleProcessorTest {
         battleCD.setFinished(false);
         battleCD.setWinner(null);
 
-        return Stream.of(Arguments.of(userA, battleAB, List.of(battleRoundBC2, battleRoundBC1, battleRoundAB1, battleRoundAB2), List.of(battleRoundAB1, battleRoundAB2)),
-                                      Arguments.of(userB, battleAB, List.of(battleRoundBC2, battleRoundBC1, battleRoundAB1, battleRoundAB2), List.of(battleRoundAB1, battleRoundAB2)),
-                         Arguments.of(userD, battleCD, List.of(battleRoundBC2, battleRoundBC1, battleRoundAB1, battleRoundAB2), List.of())
-        );
+        return Stream.of(
+                Arguments.of(
+                        userA,
+                        battleAB,
+                        List.of(battleRoundBC2, battleRoundBC1, battleRoundAB1, battleRoundAB2),
+                        List.of(battleRoundAB1, battleRoundAB2)),
+                Arguments.of(
+                        userB,
+                        battleAB,
+                        List.of(battleRoundBC2, battleRoundBC1, battleRoundAB1, battleRoundAB2),
+                        List.of(battleRoundAB1, battleRoundAB2)),
+                Arguments.of(
+                        userD,
+                        battleCD,
+                        List.of(battleRoundBC2, battleRoundBC1, battleRoundAB1, battleRoundAB2),
+                        List.of()));
+    }
 
+    public static Stream<Arguments> getBattleRound() {
+        Battle battleAB = new Battle();
+        battleAB.setPlayerParties(List.of(playerPartyA, playerPartyB));
+        battleAB.setFinished(false);
+        battleAB.setWinner(null);
+
+        BattleRound battleRoundAB1 = new BattleRound();
+        battleRoundAB1.setBattle(battleAB);
+        battleRoundAB1.setActive(false);
+        battleRoundAB1.setRoundNumber(1);
+
+        BattleRound battleRoundAB2 = new BattleRound();
+        battleRoundAB2.setBattle(battleAB);
+        battleRoundAB2.setActive(false);
+        battleRoundAB2.setRoundNumber(2);
+
+        Battle battleBC = new Battle();
+        battleBC.setPlayerParties(List.of(playerPartyB, playerPartyC));
+        battleBC.setFinished(false);
+        battleBC.setWinner(null);
+
+        BattleRound battleRoundBC1 = new BattleRound();
+        battleRoundBC1.setBattle(battleBC);
+        battleRoundBC1.setActive(false);
+        battleRoundBC1.setRoundNumber(1);
+
+        return Stream.of(
+                Arguments.of(
+                        userA,
+                        battleAB,
+                        List.of(battleRoundBC1, battleRoundAB1, battleRoundAB2),
+                        battleRoundAB2),
+                Arguments.of(
+                        userB,
+                        battleBC,
+                        List.of(battleRoundBC1, battleRoundAB1, battleRoundAB2),
+                        battleRoundBC1));
     }
 
     @ParameterizedTest
     @MethodSource("getBattleRounds")
     public void testGetRoundsByBattle(PlayerUser playerUser, Battle battle, List<BattleRound> allRounds, List<BattleRound> expectedRounds) {
-        Mockito.when(battleRoundSupplier.get()).thenReturn(allRounds);
-        BattleProcessor battleProcessor = new BattleProcessor(playerUser, battle, battleRoundSupplier, battleRoundSaver, playerPartySaver);
+        BattleProcessor battleProcessor =
+                new BattleProcessor(
+                        playerUser, battle, () -> allRounds, battleRoundSaver, playerPartySaver);
         List<BattleRound> actualBattleRounds = battleProcessor.getRoundsByBattle();
         assert actualBattleRounds.equals(expectedRounds);
     }
 
+    @ParameterizedTest
+    @MethodSource("getBattleRound")
+    public void testGetLatestRound(
+            PlayerUser playerUser,
+            Battle battle,
+            List<BattleRound> allRounds,
+            BattleRound expectedRound) {
+        BattleProcessor battleProcessor =
+                new BattleProcessor(
+                        playerUser, battle, () -> allRounds, battleRoundSaver, playerPartySaver);
+        BattleRound actualBattleRound = battleProcessor.getLatestRound();
+        assertThat(actualBattleRound).isEqualTo(expectedRound);
+    }
+
+    @Test
+    public void testGetLatestRoundEmpty() {
+
+        Battle battle = new Battle();
+        battle.setPlayerParties(List.of(playerPartyA, playerPartyB));
+        battle.setFinished(false);
+        battle.setWinner(null);
+
+        Battle battleBC = new Battle();
+        battleBC.setPlayerParties(List.of(playerPartyB, playerPartyC));
+        battleBC.setFinished(false);
+        battleBC.setWinner(null);
+
+        BattleRound battleRoundBC1 = new BattleRound();
+        battleRoundBC1.setBattle(battleBC);
+        battleRoundBC1.setActive(false);
+        battleRoundBC1.setRoundNumber(1);
+
+        BattleProcessor battleProcessor =
+                new BattleProcessor(
+                        userA,
+                        battle,
+                        () -> List.of(battleRoundBC1),
+                        battleRoundSaver,
+                        playerPartySaver);
+        assertThatThrownBy(battleProcessor::getLatestRound)
+                .isInstanceOf(NoRoundsForBattleException.class);
+    }
 
     public static BattleRound createBattleRound(List<PlayerParty> parties, Battle battle, int roundNumber, boolean isActive) {
         BattleRound battleRound = new BattleRound();
